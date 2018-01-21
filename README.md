11a869dbd30c72128a22efc71c511e8a99720554
Nitish Kumar <nitishkmrk@gmail.com>
2018-01-21 18:09:53 +0530

    Publish GitHub Page

--- README.md
+++
@@ -1,7 +0,0 @@
-# Micron.JS
-
-a [μ] microInteraction library built with CSS Animations and controlled by JavaScript Power.
-
-[Read Implementation Doc] (https://webkul.github.io/micron/docs.html)
-
-
---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var currentVersion = "1.0.3";
+    var getVersion = document.querySelectorAll(".current-version");
+    if (getVersion != undefined && getVersion != null) {
+        for (var i = 0; i < getVersion.length; i++) {
+            getVersion[i].textContent = currentVersion;
+        }
+    }
+});
